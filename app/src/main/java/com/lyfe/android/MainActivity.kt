d0ba5bcdbcbe7eed5eaf45911ad3247cfef43cba
jgeun <pjk8926@gmail.com>
2023-09-22 18:44:00 +0900

    feat: Create Bottom Navigation

--- app/src/main/java/com/lyfe/android/MainActivity.kt
+++ app/src/main/java/com/lyfe/android/MainActivity.kt
@@ -3,43 +3,26 @@ package com.lyfe.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.lyfe.android.core.navigation.navigator.LyfeNavigator
+import com.lyfe.android.ui.LyfeApp
 import com.lyfe.android.ui.theme.LyfeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+	@Inject
+	internal lateinit var lyfeNavigator: LyfeNavigator
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			LyfeTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-					Greeting("Android")
-				}
+				LyfeApp(
+					navigator = lyfeNavigator
+				)
 			}
 		}
-	}
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	LyfeTheme {
-		Greeting("Android")
 	}
 }
