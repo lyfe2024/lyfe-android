2df50cd61801bd3771dee15b6242b75b04016b35
jgeun <jgeun@wavve.com>
2023-10-17 18:07:03 +0900

    feat: draw AlarmScreen without design system

---
+++ app/src/main/java/com/lyfe/android/feature/alarm/AlarmViewModel.kt
@@ -0,0 +1,29 @@
+package com.lyfe.android.feature.alarm
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lyfe.android.core.domain.usecase.GetAlarmListUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmViewModel @Inject constructor(
+	getAlarmListUseCase: GetAlarmListUseCase,
+): ViewModel() {
+
+	var uiState by mutableStateOf<AlarmUiState>(AlarmUiState.Loading)
+		private set
+
+	init {
+		viewModelScope.launch {
+			getAlarmListUseCase().collect {
+				uiState = AlarmUiState.Failure
+//				uiState = AlarmUiState.Success(it)
+			}
+		}
+	}
+}
