2b64a5e4a6479238cd58133da9556ba6a8610975
JGeun <pjk8926@gmail.com>
2023-11-01 19:38:07 +0900

    feat: make ClickUtil.kt

---
+++ app/src/main/java/com/lyfe/android/core/common_ui/util/ClickUtil.kt
@@ -0,0 +1,24 @@
+package com.lyfe.android.core.common_ui.util
+
+interface MultipleEventsBlockManager {
+	fun processEvent(event: () -> Unit)
+
+	companion object
+}
+
+internal fun MultipleEventsBlockManager.Companion.get(): MultipleEventsBlockManager =
+	MultipleEventsBlockManagerImpl()
+
+private class MultipleEventsBlockManagerImpl : MultipleEventsBlockManager {
+	private val now: Long
+		get() = System.currentTimeMillis()
+
+	private var lastEventTimeMs: Long = 0
+
+	override fun processEvent(event: () -> Unit) {
+		if (now - lastEventTimeMs >= 300L) {
+			event.invoke()
+		}
+		lastEventTimeMs = now
+	}
+}
