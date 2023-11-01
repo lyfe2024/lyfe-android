e9e43bfa3d10352522a447db574311ca8c784a42
JGeun <pjk8926@gmail.com>
2023-11-01 19:37:50 +0900

    feat: make ModifierUtil.kt

---
+++ app/src/main/java/com/lyfe/android/core/common_ui/util/ModifierUtil.kt
@@ -0,0 +1,42 @@
+package com.lyfe.android.core.common_ui.util
+
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.semantics.Role
+
+inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
+	clickable(indication = null,
+		interactionSource = remember { MutableInteractionSource() }) {
+		onClick()
+	}
+}
+
+fun Modifier.clickableSingle(
+	enabled: Boolean = true,
+	onClickLabel: String? = null,
+	role: Role? = null,
+	onClick: () -> Unit
+) = composed(
+	inspectorInfo = debugInspectorInfo {
+		name = "clickable"
+		properties["enabled"] = enabled
+		properties["onClickLabel"] = onClickLabel
+		properties["role"] = role
+		properties["onClick"] = onClick
+	}
+) {
+	val multipleEventsCutter = remember { MultipleEventsBlockManager.get() }
+	Modifier.clickable(
+		enabled = enabled,
+		onClickLabel = onClickLabel,
+		onClick = { multipleEventsCutter.processEvent { onClick() } },
+		role = role,
+		indication = LocalIndication.current,
+		interactionSource = remember { MutableInteractionSource() }
+	)
+}
