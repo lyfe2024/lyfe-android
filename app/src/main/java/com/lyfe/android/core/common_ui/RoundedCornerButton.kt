00feb526cb17d504cb0de324d0910a839ea7f2b2
jgeun <jgeun@wavve.com>
2023-11-01 19:39:38 +0900

    feat: make common component

---
+++ app/src/main/java/com/lyfe/android/core/common_ui/RoundedCornerButton.kt
@@ -0,0 +1,48 @@
+package com.lyfe.android.core.common_ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.lyfe.android.core.common_ui.util.clickableSingle
+
+@Composable
+fun RoundedCornerButton(
+	modifier: Modifier = Modifier,
+	cornerRadius: Dp = 10.dp,
+	horizontalPadding: Dp = 0.dp,
+	verticalPadding: Dp = 0.dp,
+	isClickableColor: Color,
+	isNotClickableColor: Color = Color.Unspecified,
+	isClickable: Boolean = true,
+	onClick: () -> Unit,
+	content: @Composable () -> Unit,
+) {
+
+	Box(
+		modifier = modifier
+			.background(
+				shape = RoundedCornerShape(cornerRadius),
+				color = if (isClickable) {
+					isClickableColor
+				} else {
+					isNotClickableColor
+				}
+			)
+			.padding(vertical = verticalPadding, horizontal = horizontalPadding)
+			.clickableSingle (
+				enabled = isClickable,
+				onClick = onClick
+			),
+	) {
+		Box(modifier = Modifier.align(Alignment.Center)) {
+			content()
+		}
+	}
+}
---
+++ app/src/main/java/com/lyfe/android/core/common_ui/TextField.kt
@@ -0,0 +1,48 @@
+package com.lyfe.android.core.common_ui
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextField(
+	modifier: Modifier = Modifier,
+	singleLine: Boolean = false,
+	maxLines: Int = Int.MAX_VALUE,
+	placeHolder: @Composable () -> Unit,
+	text: String,
+	onChange: (String) -> Unit,
+) {
+	Box(
+		modifier = modifier
+	) {
+		BasicTextField(
+			modifier = Modifier
+				.fillMaxWidth()
+				.border(
+					width = 1.dp, color = Color(0xFFC4C4C4), shape = RoundedCornerShape(size = 8.dp)
+				)
+				.padding(horizontal = 12.dp, vertical = 8.dp),
+			singleLine = singleLine,
+			maxLines = maxLines,
+			value = text,
+			onValueChange = onChange
+		) {
+			if (text.isEmpty()) {
+				placeHolder()
+			} else {
+				Text(
+					text = text
+				)
+			}
+		}
+	}
+}
