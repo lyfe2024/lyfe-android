c16bfb01f714a982b242e5f3bb96a0dae7e4b570
JGeun <pjk8926@gmail.com>
2023-11-01 19:40:51 +0900

    feat: implement post screen

--- app/src/main/java/com/lyfe/android/core/navigation/LyfeNavigation.kt
+++ app/src/main/java/com/lyfe/android/core/navigation/LyfeNavigation.kt
@@ -1,6 +1,7 @@
 package com.lyfe.android.core.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import com.lyfe.android.core.navigation.navigator.LyfeNavigator
 import com.lyfe.android.feature.alarm.AlarmScreen
@@ -9,18 +10,22 @@ import com.lyfe.android.feature.post.PostScreen
 import com.lyfe.android.feature.profile.ProfileScreen
 
 fun NavGraphBuilder.lyfeHomeNavigation(
-	lyfeNavigator: LyfeNavigator
+	lyfeNavigator: LyfeNavigator,
+	navHostController: NavHostController,
 ) {
 	composable(route = LyfeScreens.Home.name) {
 		HomeScreen(navigator = lyfeNavigator)
 	}
 
 	composable(route = LyfeScreens.Post.name) {
-		PostScreen(navigator = lyfeNavigator)
+		PostScreen(
+			navigator = lyfeNavigator,
+			navHostController = navHostController,
+		)
 	}
 
 	composable(route = LyfeScreens.Alarm.name) {
-		AlarmScreen(navigator = lyfeNavigator)
+		AlarmScreen()
 	}
 
 	composable(route = LyfeScreens.Profile.name) {
