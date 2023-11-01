0b5bf74b26020eb3f3f1127d17ec8186ac4d186d
JGeun <pjk8926@gmail.com>
2023-11-01 19:32:43 +0900

    feat: make permission dialog

---
+++ app/src/main/java/com/lyfe/android/core/common_ui/permission/Permissions.kt
@@ -0,0 +1,37 @@
+package com.lyfe.android.core.common_ui.permission
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+
+@Composable
+fun PermissionsCheckScreen(
+	neededPermissions: Array<String>,
+	onPermissionCheckResult: (
+		permissionPassedList: List<NeededPermission>,
+		permissionFailedList: List<NeededPermission>
+	) -> Unit
+) {
+	val permissionPassedList = mutableListOf<NeededPermission>()
+	val permissionFailedList = mutableListOf<NeededPermission>()
+
+	val multiplePermissionLauncher = rememberLauncherForActivityResult(
+		ActivityResultContracts.RequestMultiplePermissions()
+	) { permissions ->
+		permissions.entries.forEach { entry ->
+			val neededPermission = getNeededPermission(entry.key)
+
+			if (entry.value) {
+				permissionPassedList.add(neededPermission)
+			} else {
+				permissionFailedList.add(neededPermission)
+			}
+		}
+		onPermissionCheckResult(permissionPassedList, permissionFailedList)
+	}
+
+	LaunchedEffect(Unit) {
+		multiplePermissionLauncher.launch(neededPermissions)
+	}
+}
