b4bb7126d3e4ed4a4bb92d573a6c4495a543f440
Moonsyn <msy9427@naver.com>
2023-11-11 16:47:48 +0900

    chore: add Kakao SDK library

--- app/src/main/java/com/lyfe/android/LyfeApplication.kt
+++ app/src/main/java/com/lyfe/android/LyfeApplication.kt
@@ -1,7 +1,13 @@
 package com.lyfe.android
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class LyfeApplication : Application()
+class LyfeApplication : Application() {
+	override fun onCreate() {
+		super.onCreate()
+		KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+	}
+}
