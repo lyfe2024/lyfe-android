49feef1152dbbd255ad63d89592d35086517f542
jgeun <pjk8926@gmail.com>
2023-11-16 01:36:43 +0900

    feat: implement flexible width-changing navigation

--- app/src/main/java/com/lyfe/android/ui/navigation/NavigationTab.kt
+++ app/src/main/java/com/lyfe/android/ui/navigation/NavigationTab.kt
@@ -1,7 +1,5 @@
 package com.lyfe.android.ui.navigation
 
-import android.util.Log
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
@@ -16,9 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -54,12 +51,12 @@ fun NavigationTab(
 ) {
 	val density = LocalDensity.current
 	var width by remember { mutableStateOf(0.dp) } // 너비 정보를 저장할 변수
-	val deviceType = DeviceType.getDeviceType(width)
-	val tabWidth = if(deviceType == DeviceType.LARGE) 66.dp else 30.dp
-	val itemSpacing = (width - tabWidth * items.size) / (items.size - 1)
+	val navigationShowType = NavigationShowType.getNavigationShowType(width)
+	var tabWidth by remember { mutableStateOf(0.dp) }
+	var indicatorPosition by remember { mutableStateOf(0.dp) }
 
 	val indicatorOffset: Dp by animateDpAsState(
-		targetValue = (tabWidth+itemSpacing) * selectedItemIndex,
+		targetValue = indicatorPosition,
 		animationSpec = tween(easing = LinearEasing, durationMillis = 200), label = "",
 	)
 
@@ -91,18 +88,20 @@ fun NavigationTab(
 
 				MyTabItem(
 					modifier = Modifier.onGloballyPositioned {
-						if (index == selectedItemIndex) {
-							val itemWidth = with(density) {
+						if (isSelected) {
+							indicatorPosition = with(density) {
+								it.positionInWindow().x.toDp() - 40.dp
+							}
+							tabWidth = with(density) {
 								it.size.width.toDp()
 							}
 						}
 					},
 					isSelected = isSelected,
-					deviceType = deviceType,
+					navigationShowType = navigationShowType,
 					onClick = {
 						onClick(index)
 					},
-					tabWidth = tabWidth,
 					item = items[index],
 				)
 			}
@@ -126,7 +125,7 @@ private fun MyTabIndicator(
 				x = indicatorOffset,
 			)
 			.clip(
-				shape = CircleShape,
+				shape = RoundedCornerShape(12.dp),
 			)
 			.background(
 				color = indicatorColor,
@@ -138,28 +137,13 @@ private fun MyTabIndicator(
 private fun MyTabItem(
 	modifier: Modifier = Modifier,
 	isSelected: Boolean,
-	deviceType: DeviceType = DeviceType.LARGE,
+	navigationShowType: NavigationShowType = NavigationShowType.FULL,
 	onClick: () -> Unit,
-	tabWidth: Dp,
 	item: BottomNavItem,
 ) {
-	val iconSize = if (deviceType == DeviceType.SMALL) {
-		12.dp
-	} else {
-		24.dp
-	}
-	val tabColor: Color by animateColorAsState(
-		targetValue = if (isSelected) {
-			Main500
-		} else {
-			Color.Transparent
-		},
-		animationSpec = tween(easing = LinearEasing), label = "",
-	)
-
 	Row(
-		modifier = Modifier
-			.width(tabWidth)
+		modifier = modifier
+			.padding(vertical = 6.dp, horizontal = 12.dp)
 			.clickableSingle {
 				onClick()
 			},
@@ -168,12 +152,11 @@ private fun MyTabItem(
 	) {
 
 		Image(
-//			modifier = Modifier.size(iconSize),
 			painter = painterResource(id = item.icon),
 			contentDescription = ""
 		)
 
-		if (isSelected && deviceType == DeviceType.LARGE) {
+		if (isSelected && navigationShowType == NavigationShowType.FULL) {
 			Spacer(modifier = Modifier.width(4.dp))
 
 			Text(
