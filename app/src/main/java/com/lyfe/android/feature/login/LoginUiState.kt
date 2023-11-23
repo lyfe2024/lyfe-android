b85d4deae262a6a8340b4b2470dd6537f3e66347
Moonsyn <msy9427@naver.com>
2023-11-23 16:36:54 +0900

    feat: add kakao login feature (without backend logic)

---
+++ app/src/main/java/com/lyfe/android/feature/login/LoginUiState.kt
@@ -0,0 +1,14 @@
+package com.lyfe.android.feature.login
+
+sealed interface LoginUiState {
+
+	object IDLE: LoginUiState
+
+	object Loading: LoginUiState
+
+	object Success: LoginUiState
+
+	data class Failure(
+		val reason: String?
+	) : LoginUiState
+}
