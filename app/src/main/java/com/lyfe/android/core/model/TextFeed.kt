1d5a908dcfb3271eb6e17ee00091ffe36b969545
Moonsyn <msy9427@naver.com>
2023-12-18 17:06:14 +0900

    feat: add Profile ImageFeed LazyGridColumn Component

--- app/src/main/java/com/lyfe/android/core/model/TextFeed.kt
+++ app/src/main/java/com/lyfe/android/core/model/TextFeed.kt
@@ -2,9 +2,9 @@ package com.lyfe.android.core.model
 
 data class TextFeed(
     val id: Int,
-    val title: String,
-    val content: String,
-    val createdAt: String,
-    val reactionCount: Int = 0,
-    val commentCount: Int = 0
+    val title: String = "Feed Title",
+    val content: String = "여기는 이제 고민글의 내용이 표시되는 부분입니다. 만약 제대로 표시되지 않는다면 빨리 고쳐야함.",
+    val createdAt: String = "몇 분 전",
+    val reactionCount: Int = 32,
+    val commentCount: Int = 50
 )
--- app/src/main/java/com/lyfe/android/feature/profile/ProfileUiState.kt
+++ app/src/main/java/com/lyfe/android/feature/profile/ProfileUiState.kt
@@ -8,26 +8,11 @@ sealed interface ProfileUiState {
 	object GuestSuccess : ProfileUiState
 
 	data class UserSuccess(
-		val imageFeeds: List<ImageFeed> = listOf(),
+		val imageFeeds: List<ImageFeed> = listOf(
+			ImageFeed(1), ImageFeed(2), ImageFeed(3), ImageFeed(4), ImageFeed(5), ImageFeed(6)
+		),
 		val textFeeds: List<TextFeed> = listOf(
-			TextFeed(
-				1, "title 1", "content 1 content 1 content 1 content 1 content 1 content 1", "몇 분 전", 32, 50
-			),
-			TextFeed(
-				2, "title 2", "content 2 content 1 content 1 content 1 content 1 content 1", "몇 분 전", 32, 50
-			),
-			TextFeed(
-				3, "title 3", "content 3 content 1 content 1 content 1 content 1 content 1", "몇 분 전", 32, 50
-			),
-			TextFeed(
-				4, "title 4", "content 4 content 1 content 1 content 1 content 1 content 1", "몇 분 전", 32, 50
-			),
-			TextFeed(
-				5, "title 5", "content 5 content 1 content 1 content 1 content 1 content 1", "몇 분 전", 32, 50
-			),
-			TextFeed(
-				6, "title 6", "content 6 content 1 content 1 content 1 content 1 content 1", "몇 분 전", 32, 50
-			)
+			TextFeed(1), TextFeed(2), TextFeed(3), TextFeed(4), TextFeed(5), TextFeed(6),TextFeed(7),
 		)
 	) : ProfileUiState
 
