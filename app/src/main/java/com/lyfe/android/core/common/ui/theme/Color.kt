f5131492e652dfd48ae998c12abcf00142d3bada
jgeun <pjk8926@gmail.com>
2023-12-22 23:52:39 +0900

    chore: move to common ui package

--- app/src/main/java/com/lyfe/android/core/common/ui/theme/Color.kt
+++ app/src/main/java/com/lyfe/android/core/common/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.lyfe.android.ui.theme
+package com.lyfe.android.core.common.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/lyfe/android/core/common/ui/theme/Type.kt
+++ app/src/main/java/com/lyfe/android/core/common/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.lyfe.android.ui.theme
+package com.lyfe.android.core.common.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
