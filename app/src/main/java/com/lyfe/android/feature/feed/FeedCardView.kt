5f5f4ae12a2f58411f51858fcca48fa6a5586d91
jgeun <pjk8926@gmail.com>
2023-12-22 23:56:49 +0900

    feat: implement FeedScreen

--- app/src/main/java/com/lyfe/android/feature/feed/FeedCardView.kt
+++ app/src/main/java/com/lyfe/android/feature/feed/FeedCardView.kt
@@ -1,52 +1,44 @@
 package com.lyfe.android.feature.feed
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.lyfe.android.R
+import com.lyfe.android.core.common.ui.component.LyfeCardViewDesignType
+import com.lyfe.android.core.common.ui.component.LyfeFeedCardView
+import com.lyfe.android.core.common.ui.theme.TempColor
 import com.lyfe.android.core.model.Feed
-import com.lyfe.android.ui.theme.BlackTransparent30
-import com.lyfe.android.ui.theme.TempColor
 
 @Composable
-fun FeedCardView(
+fun FeedScreenCardView(
 	modifier: Modifier = Modifier,
 	feed: Feed
 ) {
-	Column {
-		FeedMainBox(
-			modifier = modifier,
-			feed = feed
+	Column(
+		modifier = modifier
+	) {
+		LyfeFeedCardView(
+			modifier = Modifier.wrapContentSize(),
+			feed = feed,
+			designType = LyfeCardViewDesignType.FEED_SCREEN_CARD
 		)
 		Spacer(modifier = Modifier.height(4.dp))
 		FeedScoreRow(
@@ -68,6 +60,7 @@ private fun FeedScoreRow(
 		verticalAlignment = Alignment.CenterVertically
 	) {
 		Image(
+			modifier = Modifier.padding(0.5.dp).size(16.dp),
 			painter = painterResource(id = R.drawable.ic_whisky),
 			contentDescription = "ic_whisky"
 		)
@@ -87,6 +80,7 @@ private fun FeedScoreRow(
 		Spacer(modifier = Modifier.width(8.dp))
 
 		Image(
+			modifier = Modifier.padding(0.66667.dp).size(16.dp),
 			painter = painterResource(id = R.drawable.ic_comment),
 			contentDescription = "ic_whisky"
 		)
@@ -105,90 +99,9 @@ private fun FeedScoreRow(
 	}
 }
 
-@Composable
-@OptIn(ExperimentalGlideComposeApi::class)
-private fun FeedMainBox(
-	modifier: Modifier,
-	feed: Feed
-) {
-	Box(
-		modifier = modifier
-			.widthIn(min = 152.dp)
-			.aspectRatio(getMainBoxRatio())
-			.fillMaxSize()
-			.clip(RoundedCornerShape(16.dp))
-	) {
-		GlideImage(
-			modifier = Modifier
-				.fillMaxSize()
-				.clip(RoundedCornerShape(16.dp))
-				.align(Alignment.Center),
-			model = feed.feedImageUrl,
-			contentDescription = "feed_image",
-			contentScale = ContentScale.FillBounds
-		)
-
-		Spacer(
-			modifier = Modifier
-				.fillMaxSize()
-				.background(BlackTransparent30)
-		)
-
-		Row(
-			modifier = Modifier.padding(10.dp),
-			horizontalArrangement = Arrangement.Center,
-			verticalAlignment = Alignment.CenterVertically
-		) {
-			GlideImage(
-				modifier = Modifier
-					.clip(CircleShape)
-					.size(24.dp),
-				model = feed.userProfileImgUrl,
-				contentDescription = "feed_profile_image",
-				contentScale = ContentScale.Crop
-			)
-
-			Spacer(modifier = Modifier.width(4.dp))
-
-			Text(
-				modifier = Modifier.weight(1f),
-				text = feed.userName,
-				style = TextStyle(
-					fontSize = 12.sp,
-					lineHeight = 18.sp,
-					fontWeight = FontWeight.W600,
-					color = Color.White
-				),
-				overflow = TextOverflow.Ellipsis
-			)
-		}
-
-		Text(
-			modifier = Modifier
-				.fillMaxWidth()
-				.align(Alignment.BottomCenter)
-				.padding(10.dp),
-			text = feed.title,
-			style = TextStyle(
-				color = Color.White,
-				fontWeight = FontWeight.W700,
-				fontSize = 14.sp,
-				lineHeight = 22.sp
-			),
-			overflow = TextOverflow.Ellipsis
-		)
-	}
-}
-
-private fun getMainBoxRatio(): Float {
-	val fractionWidth = 152f
-	val fractionHeight = 210f
-	return fractionWidth / fractionHeight
-}
-
 @Preview
 @Composable
-private fun Preview_FeedCardView() {
+private fun Preview_FeedScreenCardView() {
 	val feed = Feed(
 		feedId = 1L,
 		title = "타이틀1",
@@ -202,8 +115,8 @@ private fun Preview_FeedCardView() {
 		commentCount = 1,
 		isLike = false
 	)
-
-	FeedCardView(
+	
+	FeedScreenCardView(
 		modifier = Modifier.fillMaxSize(),
 		feed = feed
 	)
