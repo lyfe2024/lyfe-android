f64567be66b039ba6b85f1cf0ff6f9e82f12b7db
Moonsyn <msy9427@naver.com>
2024-02-16 18:15:56 +0900

    chore: TokenRepository, UserRepository 분리

--- app/src/main/java/com/lyfe/android/core/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/lyfe/android/core/data/repository/UserRepositoryImpl.kt
@@ -1,51 +1,16 @@
 package com.lyfe.android.core.data.repository
 
-import com.lyfe.android.core.data.datasource.LocalTokenDataSource
 import com.lyfe.android.core.data.datasource.RemoteUserDataSource
 import com.lyfe.android.core.data.model.CheckNicknameResponse
 import com.lyfe.android.core.data.network.model.Result
 import com.lyfe.android.core.domain.repository.UserRepository
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-	private val remoteUserDataSource: RemoteUserDataSource,
-	private val localTokenDataSource: LocalTokenDataSource
+	private val remoteUserDataSource: RemoteUserDataSource
 ) : UserRepository {
 
 	override suspend fun fetchIsNicknameDuplicated(nickname: String): Result<CheckNicknameResponse> {
 		return remoteUserDataSource.checkNicknameDuplicated(nickname)
-	}
-
-	override suspend fun updateSignUpToken(signUpToken: String) {
-		localTokenDataSource.updateSignUpToken(signUpToken)
-	}
-
-	override suspend fun updateAccessToken(accessToken: String) {
-		localTokenDataSource.updateAccessToken(accessToken)
-	}
-
-	override suspend fun updateRefreshToken(refreshToken: String) {
-		localTokenDataSource.updateRefreshToken(refreshToken)
-	}
-
-	override fun getSignUpToken(): Flow<String> {
-		return localTokenDataSource.getSignUpToken()
-	}
-
-	override fun getAccessToken(): Flow<String> {
-		return localTokenDataSource.getAccessToken()
-	}
-
-	override fun getRefreshToken(): Flow<String> {
-		return localTokenDataSource.getRefreshToken()
-	}
-
-	override suspend fun deleteSignUpToken() {
-		localTokenDataSource.deleteSignUpToken()
-	}
-
-	override suspend fun deleteAllToken() {
-		localTokenDataSource.deleteAllToken()
 	}
 }
--- app/src/main/java/com/lyfe/android/core/domain/usecase/UpdateAccessTokenUseCase.kt
+++ app/src/main/java/com/lyfe/android/core/domain/usecase/UpdateAccessTokenUseCase.kt
@@ -1,11 +1,11 @@
 package com.lyfe.android.core.domain.usecase
 
-import com.lyfe.android.core.domain.repository.UserRepository
+import com.lyfe.android.core.domain.repository.TokenRepository
 import javax.inject.Inject
 
 class UpdateAccessTokenUseCase @Inject constructor(
-	private val userRepository: UserRepository
+	private val tokenRepository: TokenRepository
 ) {
 	// 유저 액세스 토큰 저장
-	suspend operator fun invoke(accessToken: String) = userRepository.updateAccessToken(accessToken)
+	suspend operator fun invoke(accessToken: String) = tokenRepository.updateAccessToken(accessToken)
 }
