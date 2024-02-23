ada10a238438e843331b52a26034b4fc2148eeef
Moonsyn <msy9427@naver.com>
2024-02-23 15:23:30 +0900

    chore: change Body Object From Map to Request Data Class

--- app/src/main/java/com/lyfe/android/core/data/network/service/AuthService.kt
+++ app/src/main/java/com/lyfe/android/core/data/network/service/AuthService.kt
@@ -1,7 +1,10 @@
 package com.lyfe.android.core.data.network.service
 
+import com.lyfe.android.core.data.model.AuthUserRequest
 import com.lyfe.android.core.data.model.AuthUserResponse
+import com.lyfe.android.core.data.model.PostUserRequest
 import com.lyfe.android.core.data.model.PostUserResponse
+import com.lyfe.android.core.data.model.ReissueTokenRequest
 import com.lyfe.android.core.data.model.ReissueTokenResponse
 import com.lyfe.android.core.data.network.model.Result
 import retrofit2.http.Body
@@ -11,13 +14,13 @@ interface AuthService {
 
 	// 회원가입 API
 	@POST("/v1/auth/join")
-	suspend fun postUser(@Body body: Map<String, String>): Result<PostUserResponse>
+	suspend fun postUser(@Body body: PostUserRequest): Result<PostUserResponse>
 
 	// 소셜 로그인 API
 	@POST("/v1/auth/login")
-	suspend fun authUser(@Body body: Map<String, String>): Result<AuthUserResponse>
+	suspend fun authUser(@Body body: AuthUserRequest): Result<AuthUserResponse>
 
 	// 토큰 재발행 API
 	@POST("/v1/auth/reissue")
-	suspend fun reissueToken(@Body body: Map<String, String>): Result<ReissueTokenResponse>
+	suspend fun reissueToken(@Body body: ReissueTokenRequest): Result<ReissueTokenResponse>
 }
