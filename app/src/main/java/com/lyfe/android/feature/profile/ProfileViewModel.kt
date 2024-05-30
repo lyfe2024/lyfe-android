d0cdcd6a63688e929a00151a6521d7c7beea9f3d
Moonsyn <msy9427@naver.com>
2024-05-30 23:32:33 +0900

    fix: 토큰 재발급 오류 해결

--- app/src/main/java/com/lyfe/android/feature/profile/ProfileViewModel.kt
+++ app/src/main/java/com/lyfe/android/feature/profile/ProfileViewModel.kt
@@ -2,6 +2,7 @@ package com.lyfe.android.feature.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.lyfe.android.core.domain.usecase.GetAccessTokenUseCase
 import com.lyfe.android.core.domain.usecase.GetUserInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,7 +13,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-	private val getUserInfoUseCase: GetUserInfoUseCase
+	private val getUserInfoUseCase: GetUserInfoUseCase,
+	private val getAccessTokenUseCase: GetAccessTokenUseCase
 ) : ViewModel() {
 
 	private val _uiState = MutableStateFlow<ProfileUiState>(ProfileUiState.IDLE)
@@ -23,11 +25,15 @@ class ProfileViewModel @Inject constructor(
 	}
 
 	private fun getUserInfo() = viewModelScope.launch {
-		// TODO 게스트 여부 체크 (토큰 확인)
-		getUserInfoUseCase().catch {
-			_uiState.emit(ProfileUiState.Error(message = it.message))
-		}.collect {
-			_uiState.emit(ProfileUiState.UserLoaded(it))
+		getAccessTokenUseCase().collect { token ->
+			if (token.isNullOrEmpty()) {
+				return@collect
+			}
+			getUserInfoUseCase().catch {
+				_uiState.emit(ProfileUiState.Error(message = it.message))
+			}.collect {
+				_uiState.emit(ProfileUiState.UserLoaded(it))
+			}
 		}
 	}
 }
