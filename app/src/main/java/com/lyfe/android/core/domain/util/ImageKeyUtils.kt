5ee7362fa8a2f6ec39786ef927040cc965627656
jgeun <pjk8926@gmail.com>
2024-06-30 22:40:35 +0900

    fix: apply ktlint

--- app/src/main/java/com/lyfe/android/core/domain/util/ImageKeyUtils.kt
+++ app/src/main/java/com/lyfe/android/core/domain/util/ImageKeyUtils.kt
@@ -3,5 +3,6 @@ package com.lyfe.android.core.domain.util
 import com.lyfe.android.BuildConfig
 import com.lyfe.android.core.model.ImageKey
 
-fun getImageServerUrl(imageKey: ImageKey)
-	= "${BuildConfig.AWS_BASE_URL}/${imageKey.removeLeadingSlash()}"
+fun getImageServerUrl(imageKey: ImageKey): String {
+	return "${BuildConfig.AWS_BASE_URL}/${imageKey.removeLeadingSlash()}"
+}
